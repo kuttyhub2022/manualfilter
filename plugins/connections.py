5be11668d0b9142c8db3b956aebca937b32c5b8d
kuttyhub2022 <106232707+kuttyhub2022@users.noreply.github.com>
2022-08-16 11:08:32 +0530

    Update connections.py

--- plugins/connections.py
+++ plugins/connections.py
@@ -12,9 +12,12 @@ from database.connections_mdb import add_connection, all_connections, if_active,
 
 
 
-@Client.on_message((filters.private | filters.group) & filters.command(Config.CONNECT_COMMAND))
-async def addconnection(client,message):
-    userid = message.from_user.id
+
+@Client.on_message((filters.private | filters.group) & filters.command('connect'))
+async def addconnection(client, message):
+    userid = message.from_user.id if message.from_user else None
+    if not userid:
+        return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
     chat_type = message.chat.type
 
     if chat_type == "private":
@@ -29,24 +32,26 @@ async def addconnection(client,message):
             )
             return
 
-    elif (chat_type == "group") or (chat_type == "supergroup"):
+    elif chat_type in ["group", "supergroup"]:
         group_id = message.chat.id
 
     try:
         st = await client.get_chat_member(group_id, userid)
-        if (st.status == "administrator") or (st.status == "creator") or (str(userid) in Config.AUTH_USERS):
-            pass
-        else:
+        if (
+                st.status != "administrator"
+                and st.status != "creator"
+                and userid not in ADMINS
+        ):
             await message.reply_text("You should be an admin in Given group!", quote=True)
             return
     except Exception as e:
-        print(e)
+        logger.exception(e)
         await message.reply_text(
             "Invalid Group ID!\n\nIf correct, Make sure I'm present in your group!!",
-            quote=True
+            quote=True,
         )
-        return
 
+        return
     try:
         st = await client.get_chat_member(group_id, "me")
         if st.status == "administrator":
@@ -56,11 +61,11 @@ async def addconnection(client,message):
             addcon = await add_connection(str(group_id), str(userid))
             if addcon:
                 await message.reply_text(
-                    f"Sucessfully connected to **{title}**\nNow manage your group from my pm !",
+                    f"Successfully connected to **{title}**\nNow manage your group from my pm !",
                     quote=True,
                     parse_mode="md"
                 )
-                if (chat_type == "group") or (chat_type == "supergroup"):
+                if chat_type in ["group", "supergroup"]:
                     await client.send_message(
                         userid,
                         f"Connected to **{title}** !",
@@ -74,27 +79,30 @@ async def addconnection(client,message):
         else:
             await message.reply_text("Add me as an admin in group", quote=True)
     except Exception as e:
-        print(e)
-        await message.reply_text(
-            "Some error occured! Try again later.",
-            quote=True
-        )
+        logger.exception(e)
+        await message.reply_text('Some error occurred! Try again later.', quote=True)
         return
 
 
-@Client.on_message((filters.private | filters.group) & filters.command(Config.DISCONNECT_COMMAND))
-async def deleteconnection(client,message):
-    userid = message.from_user.id
+@Client.on_message((filters.private | filters.group) & filters.command('disconnect'))
+async def deleteconnection(client, message):
+    userid = message.from_user.id if message.from_user else None
+    if not userid:
+        return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
     chat_type = message.chat.type
 
     if chat_type == "private":
         await message.reply_text("Run /connections to view or disconnect from groups!", quote=True)
 
-    elif (chat_type == "group") or (chat_type == "supergroup"):
+    elif chat_type in ["group", "supergroup"]:
         group_id = message.chat.id
 
         st = await client.get_chat_member(group_id, userid)
-        if not ((st.status == "administrator") or (st.status == "creator") or (str(userid) in Config.AUTH_USERS)):
+        if (
+                st.status != "administrator"
+                and st.status != "creator"
+                and str(userid) not in ADMINS
+        ):
             return
 
         delcon = await delete_connection(str(userid), str(group_id))
@@ -105,7 +113,7 @@ async def deleteconnection(client,message):
 
 
 @Client.on_message(filters.private & filters.command(["connections"]))
-async def connections(client,message):
+async def connections(client, message):
     userid = message.from_user.id
 
     groupids = await all_connections(str(userid))
@@ -121,14 +129,11 @@ async def connections(client,message):
             ttl = await client.get_chat(int(groupid))
             title = ttl.title
             active = await if_active(str(userid), str(groupid))
-            if active:
-                act = " - ACTIVE"
-            else:
-                act = ""
+            act = " - ACTIVE" if active else ""
             buttons.append(
                 [
                     InlineKeyboardButton(
-                        text=f"{title}{act}", callback_data=f"groupcb:{groupid}:{title}:{act}"
+                        text=f"{title}{act}", callback_data=f"groupcb:{groupid}:{act}"
                     )
                 ]
             )
@@ -138,5 +143,10 @@ async def connections(client,message):
         await message.reply_text(
             "Your connected group details ;\n\n",
             reply_markup=InlineKeyboardMarkup(buttons),
+            quote=True
+        )
+    else:
+        await message.reply_text(
+            "There are no active connections!! Connect to some groups first.",
             quote=True
         )
